c13d0beabab6a528064d56862e9d25f7938ff9b9
Romain Doulaud <romain.doulaud@epita.fr>
2025-04-22 17:51:02 +0200

    update metro config

---
+++ app.config.ts
@@ -0,0 +1,9 @@
+import 'ts-node/register'; // Add this to import TypeScript files
+import { ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+  name: 'SwAppart',
+  slug: 'SwAppart',
+};
+
+export default config;
