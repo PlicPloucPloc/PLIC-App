28504f9cd23a127bf6a80283d0d714cf9e5ec9f6
Romain Doulaud <romain.doulaud@epita.fr>
2025-07-01 16:35:46 +0200

    updated to new backend

--- src/app/definitions/rest/ApartmentService.ts
+++ src/app/definitions/rest/ApartmentService.ts
@@ -1,40 +1,18 @@
 export interface ApartmentResponse {
-  adLink: string;
+  apartment_id: number;
+  name: string;
   location: string;
-  additional_info: AdditionalInfo;
-}
-
-export interface AdditionalInfo {
-  title: string;
-  description: string;
-  criteria: Criteria;
-  images: Images;
-}
-
-export interface Criteria {
-  type: string;
-  isFurnished: boolean;
+  is_furnished: boolean;
   surface: number;
-  numberOfRooms: number;
-  numberOfBedRoom: number;
-  energyClass: string;
+  energy_class: string;
+  available_from: string;
+  rent: number;
+  type: string;
   ges: string;
-  additionalData: string;
-  heating_type: string;
-  heatingMode: string;
+  description: string;
+  number_of_rooms: number;
+  number_of_bed_rooms: number;
   floor: number;
   elevator: boolean;
-  availableFrom: string;
-  monthlyCharges: number;
-  securityDeposite: number;
-  includeCharges: boolean;
-}
-
-export interface Images {
-  thumb_url: string;
-  small_url: string;
-  nb_images: number;
-  urls: string[];
-  urls_thumb: string[];
-  urls_large: string[];
+  parking_spaces: number;
 }
